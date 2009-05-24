9dce12ce9785ddf0f1214343546096d631a52049
Charles Childers <crc.platypus@gmail.com>
2009-05-24 11:17:00 -0400

    started checking in old files

---
+++ cat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+#define MAX_BUFFER 4096
+
+int main(int argc, char *argv[])
+{
+  int file = 0;
+  int i = 1, count = 0;
+  char buffer[MAX_BUFFER];
+
+  if (!(--argc))
+    goto loop;
+
+main_cat_loop:
+  if ((file = open(argv[i++], O_RDONLY)) < 0)
+    return 1;
+
+loop:
+  if (count = read(file, buffer, MAX_BUFFER))
+  {
+    printf("%s", buffer);
+    goto loop;
+  }
+
+  close(file);
+
+  if (i <= argc)
+    goto main_cat_loop;
+
+  return 0;
+}
---
+++ ls.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+main(int argc, char *argv[])
+{
+  struct dirent **names;
+  int n, i;
+  if (argc < 2)
+    argv[1] = ".";
+  n = scandir(argv[1], &names, 0, alphasort);
+  if (n < 0)
+    perror("scandir");
+  else
+  {
+    for (i = 0; i < n; i++)
+    {
+      printf("%s\t(%d)\n", names[i]->d_name, size(names[i]->d_name));
+      free(names[i]);
+    }
+    free(names);
+  }
+}
+
+
+size(char *fn)
+{
+  struct stat buf;
+  stat(fn, &buf);
+  return buf.st_size;
+}
